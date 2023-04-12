80f5fc4bcee4fdac97251ebd278a9545421a7a4d
Darlan Schwartz <darlanswtz@gmail.com>
2023-04-12 12:52:24 -0300

    Mensagens sendo carregadas e atualizadas do servidor e também adicionada a possibilidade de enviar mensagens e receber mensagens privadas também ja acontece o bonus de scroll into view

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 axios.defaults.headers.common['Authorization'] = 'sI7b4Z8QE5opnAc5PF2Xgwuz';
 const userInputName =  document.querySelector(".login-username");
+const userInputMessage = document.querySelector(".input-msg-text");
+
 let statusInterval = null;
+let messagesInterval = null;
 let myUsername = null;
 
 let visibilityState = 'public';
 let receivers = 'all';
 
-let usersLoggedIn = [];
+const msgURL = "https://mock-api.driven.com.br/api/vm/uol/messages";
+const loginURL = "https://mock-api.driven.com.br/api/vm/uol/participants";
+const statusURL = "https://mock-api.driven.com.br/api/vm/uol/status";
 
 function CheckLoginEvent(event){
 
@@ -30,7 +35,7 @@ function TryLogin()
     document.querySelector(".login-btn").classList.add('hidden');
     document.querySelector(".login-username").classList.add('hidden');
 
-    let answer = axios.post("https://mock-api.driven.com.br/api/vm/uol/participants ",loginObj);
+    let answer = axios.post(loginURL,loginObj);
     answer.then (Login);
 
     answer.catch(ErrorFix);
@@ -46,8 +51,8 @@ function Login(data)
     document.querySelector(".messages-container").classList.remove("hidden");
     document.querySelector(".send-msg-container").classList.remove("hidden");
     statusInterval = setInterval(UpdateStatus, 5000);
-    console.log(data);
-    //document.querySelector(".visibility-settings-window").classList.remove("hidden");
+    messages = setInterval(GetServerMessages,3000);
+    GetServerMessages();
 }
 
 function ErrorFix(error)
@@ -65,10 +70,9 @@ function ErrorFix(error)
 
 function UpdateStatus()
 {
-    const status = axios.post("https://mock-api.driven.com.br/api/vm/uol/status", myUsername);
+    const status = axios.post(statusURL, myUsername);
     status.then (LogStatus);
 }
-
 
 function LogStatus(data)
 {
@@ -86,7 +90,6 @@ function CloseVisibilitySettings()
     document.querySelector(".visibility-settings-window").classList.toggle('open');    
     document.querySelector(".modal-background").classList.add('hidden');
 }
-
 
 function SetVisibilityState(state)
 {
@@ -114,21 +117,123 @@ function SetReceiver(receiver)
     {
         receivers = receiver;
     }
+    else
+    {
+        receivers = receiver.querySelector('p').innerHTML;
+    }
+
+    console.log(receivers);
 
     const allBtn = document.querySelector(".all-users-btn");
-
-    const userButton = document.querySelector(".private-btn");
 
     if(receiver === "all")
     {
         allBtn.querySelector('.checkmark').classList.remove('hidden');
-        userButton.querySelector('.checkmark').classList.add('hidden');
+        const allUsers = document.querySelectorAll(".user-btn");
+        allUsers.forEach(user =>{
+            user.querySelector('.checkmark').classList.add('hidden');
+        });
     }
     else
     {
-        publicBtn.querySelector('.checkmark').classList.add('hidden');
-        privateBtn.querySelector('.checkmark').classList.remove('hidden');
+        allBtn.querySelector('.checkmark').classList.add('hidden');
+
+        const allUsers = document.querySelectorAll(".user-btn");
+        allUsers.forEach(user =>{
+            user.querySelector('.checkmark').classList.add('hidden');
+        });
+
+        receiver.querySelector('.checkmark').classList.remove('hidden');
     }
 
     console.log(visibilityState);
+}
+
+function SendMessageEvent(event)
+{
+    if(event.key === "Enter")
+    {
+        SendMessage();
+    }
+}
+
+function SendMessage()
+{
+    const msgText = document.querySelector(".input-msg-text").value;
+
+    if(msgText === null || msgText === "")
+    {
+        return;
+    }
+
+    const sendingTo = receivers == 'all' ? 'Todos' : receivers;
+    const messageType = visibilityState == 'public' ? "message" : "private_message";
+
+    const msgObject = 
+    {
+        from: myUsername.name,
+        to: sendingTo,
+        text: msgText,
+        type: messageType // ou "private_message" para o bônus
+    }
+
+    const SendMessagePromise = axios.post(msgURL,msgObject);
+
+    document.querySelector(".input-msg-text").value = '';
+
+    SendMessagePromise.then(GetServerMessages);
+}
+
+function GetServerMessages()
+{
+    const messages = axios.get("https://mock-api.driven.com.br/api/vm/uol/messages");
+    messages.then(UpdateMessages);
+}
+
+function UpdateMessages(allMessages)
+{
+    console.log(allMessages);
+    document.querySelector(".messages-container").innerHTML = '';
+    
+
+    allMessages.data.forEach( msg =>{
+
+        const isStatus = msg.type === "status" ? "msg-box status-msg" : "msg-box";
+
+        if(msg.to === "Todos")
+        {
+            document.querySelector(".messages-container").innerHTML +=
+            `
+            <div class="${isStatus}">
+                <div class="msg-time">${msg.time}</div>
+                <div class="msg-username">${msg.from}</div>
+                <div class="msg-content">${msg.text}</div>
+            </div>
+        `;
+        }
+        else
+        {
+            if(msg.to === myUsername.name)
+            {
+                let privateMsgFormat = "reservadamente para " + myUsername.name +  msg.text;
+                document.querySelector(".messages-container").innerHTML +=
+                `
+                <div class="msg-box private-msg">
+                    <div class="msg-time">${msg.time}</div>
+                    <div class="msg-username">${msg.from}</div>
+                    <div class="msg-content">${privateMsgFormat}</div>
+                </div>
+                `;
+            }
+        }
+    });
+
+    const div = document.querySelector(".messages-container");
+    const lastChild = div.lastElementChild;
+    lastChild.scrollIntoView();
+}
+
+function GetUsers()
+{
+
 }
