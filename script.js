0a70d840adb8f8616831fc16e39274cecd8467eb
Darlan Schwartz <darlanswtz@gmail.com>
2023-04-13 17:40:23 -0300

    Todo código comentado e resolvido um problema de layout que não aparecia que a mensagem estava sendo enviada para todos quando o to: era Todos

--- script.js
+++ script.js
@@ -1,21 +1,31 @@
+// Add my hub token to every get from axios
 axios.defaults.headers.common['Authorization'] = 'sI7b4Z8QE5opnAc5PF2Xgwuz';
+// Login inputfield
 const userInputName =  document.querySelector(".login-username");
+// Message Inputfield
 const userInputMessage = document.querySelector(".input-msg-text");
 
+// Update intervals id's
 let statusInterval = null;
 let messagesInterval = null;
 let usersInterval = null;
+
+// My registered login username
 let myUsername = null;
 
+// Visibility of my message
 let visibilityState = 'public';
+
+// Who will receive my message
 let receivers = 'all';
 
+// Url's of api axios
 const msgURL = "https://mock-api.driven.com.br/api/vm/uol/messages";
 const loginURL = "https://mock-api.driven.com.br/api/vm/uol/participants";
 const statusURL = "https://mock-api.driven.com.br/api/vm/uol/status";
 
 
-
+// Check if i pressed enter inside the login inputfield and try login
 function CheckLoginEvent(event){
 
     if(event.key === "Enter")
@@ -26,20 +36,28 @@ function CheckLoginEvent(event){
 
 function TryLogin()
 {
+    // Check if the username is valid
     if(userInputName.value === '' || userInputName.value === null)
     {
         return;
     }
     
+    // Create a object that the api accepts using the input value of the login inputfield as the "name" attribute of this object
     const loginObj = { name: userInputName.value };
     
+    // Show login-in gif and text
     document.querySelector(".loading-gif").classList.remove('hidden');
     document.querySelector(".login-screen p").classList.remove('hidden');
+
+    // Hide inputfield and login button of username
     document.querySelector(".login-btn").classList.add('hidden');
     document.querySelector(".login-username").classList.add('hidden');
 
+    // Ask server if i can login with this object.name
     let answer = axios.post(loginURL,loginObj);
+    // If i can login : Login
     answer.then(Login);
+    // Else i ask for another name
     answer.catch(LoginError);
 }
 
@@ -48,64 +66,92 @@ function Login()
     GetServerMessages();
     GetUsers();
     
+    // Start updating messages users and send my logged in status
     statusInterval = setInterval(GetUsers, 10000);
-    messagesInterval = setInterval(GetServerMessages,3000); //alterado
+    messagesInterval = setInterval(GetServerMessages,3000);
     usersInterval =setInterval(UpdateStatus,3000);
 
+    // Assign my world var to this object below
     myUsername = { name: userInputName.value };
+    
+    //Clear the user inputfield name value
     userInputName.value = null;
+
+    // Close the login screen
     document.querySelector(".login-screen").classList.add("hidden");
+    
+    // Show the header messages and send messages UI container
     document.querySelector(".header-container").classList.remove("hidden");
     document.querySelector(".messages-container").classList.remove("hidden");
     document.querySelector(".send-msg-container").classList.remove("hidden");
 
+    // Set the default receiver of the messages to all / Todos
     SetReceiver('all');
+
+    // Set the visibility of my message to default public / message
     SetVisibilityState('public');
 }
 
+// Catch login error
 function LoginError(error)
 {
+    // Log the erros on console
     console.log("Status code: " + error.response.status); // Ex: 404
 	console.log("Mensagem de erro: " + error.response.data); // Ex: Not Found
 
+    // Hide loggin-in gif and text
     document.querySelector(".loading-gif").classList.add('hidden');
     document.querySelector(".login-screen p").classList.add('hidden');
+
+    // Show login button and inputfield
     document.querySelector(".login-btn").classList.remove('hidden');
     document.querySelector(".login-username").classList.remove('hidden');
 
+    // Ask for another name
     alert('Digite outro nome');
 }
 
 function ReturnToLogin()
 {
+    // Reload page
     window.location.reload();
 }
 
 function UpdateStatus()
 {
+    // Send my current logged in status
     const userStatus = axios.post(statusURL, myUsername);
+
+    // Reload page if i there is and error on updating my connection with the server
     userStatus.catch(ReturnToLogin);
 }
 function OpenVisibilitySettings()
 {
+    // Show Sidebar visibility setting on click users button on header and show alpha black background
     document.querySelector(".visibility-settings-window").classList.toggle('open');
     document.querySelector(".modal-background").classList.remove('hidden');
 }
 
 function CloseVisibilitySettings()
 {
+    // Close visibility settings on click on alpha black background
     document.querySelector(".visibility-settings-window").classList.toggle('open');    
     document.querySelector(".modal-background").classList.add('hidden');
 }
 
 function SetVisibilityState(state)
 {
+    // Set the visibility stat on click button of the visibility settings window
     visibilityState = state;
+
+    // Get the public and private buttons of the visibility setting window
     const publicBtn = document.querySelector(".public-btn");
     const privateBtn = document.querySelector(".private-btn");
 
+    // Update the text informing the user to wich person the message will go and for who else it will appear
     if(receivers == 'all')
     {
+        // If the receivers are all update to this below
         document.querySelector(".sending-to-info").innerHTML = 
         `
             Enviando mensagem para (Todos)
@@ -113,8 +159,10 @@ function SetVisibilityState(state)
     }
     else
     {
+        // If the receivers are no all and the visibility is set to public
         if(visibilityState == 'public')
         {
+            // If the visibility is set to public update to this below
             document.querySelector(".sending-to-info").innerHTML = 
             `
                 Enviando mensagem para ${receivers} (Publico)
@@ -122,6 +170,7 @@ function SetVisibilityState(state)
         }
         else if( visibilityState == 'private')
         {
+            // If the visibility is set to private update to this below
             document.querySelector(".sending-to-info").innerHTML = 
             `
                 Enviando mensagem para ${receivers} (Reservadamente)
@@ -129,6 +178,7 @@ function SetVisibilityState(state)
         }
     }
 
+    // Update checkmarks of the visibility state changes on click
     if(state === "public")
     {
         publicBtn.querySelector('.checkmark').classList.remove('hidden');
@@ -194,6 +244,7 @@ function SetReceiver(receiver)
     }
 }
 
+// Check send message event on press enter inside the inputfield of input-message-text
 function SendMessageEvent(event)
 {
     if(event.key === "Enter")
@@ -202,21 +253,28 @@ function SendMessageEvent(event)
     }
 }
 
+// Try to send message
 function SendMessage()
 {
+    // Get the message text from the input field of the input-message-text
     const msgText = document.querySelector(".input-msg-text").value;
 
+    // Return if the message is empty or null
     if(msgText === null || msgText === "")
     {
         return;
     }
 
+    // Get the corret receivers
     const sendingTo = receivers == 'all' ? 'Todos' : receivers;
+
+    // Get the visibility of the message (public / private)
     const correctMessageType = receivers == 'all' ? "message" : "private_message";
+
+    // If the message if public continues as a normal message else set as resevated message
     const messageType = visibilityState == 'public' ? "message" : correctMessageType;
 
-    console.log(messageType);
-
+    // Create message object
     const msgObject = 
     {
         from: myUsername.name,
@@ -225,45 +283,74 @@ function SendMessage()
         type: messageType
     }
 
+    // Send and save message axios promise
     const SendMessagePromise = axios.post(msgURL,msgObject);
 
+    // Clear the inputfield text
     document.querySelector(".input-msg-text").value = '';
 
+    // Update messages if the send was sucessfull
     SendMessagePromise.then(GetServerMessages);
+    
+    // Return to login if there was an error
     SendMessagePromise.catch(ReturnToLogin);
 }
 
 function GetServerMessages()
 {
-    const messages = axios.get("https://mock-api.driven.com.br/api/vm/uol/messages");
+    // Get messages from server and update if sucess or return to login if error
+    const messages = axios.get(msgURL);
     messages.then(UpdateMessages);
     messages.catch(ReturnToLogin);
 }
 
 function UpdateMessages(allMessages)
 {
+    // Clear all messages
     document.querySelector(".messages-container").innerHTML = '';
 
+    // Foreach message i receive back from the server
     allMessages.data.forEach( msg =>{
 
+        // Check if it is and status message to add gray background
         const isStatus = msg.type === "status" ? "msg-box status-msg" : "msg-box";
-
+        
+        // If the message is for Todos just render it on the chat
         if(msg.to === "Todos")
         {
-            document.querySelector(".messages-container").innerHTML +=
-            `
-            <div data-test="message" class="${isStatus}">
-                <div class="msg-time">${msg.time}</div>
-                <div class="msg-username">${msg.from}</div>
-                <div class="msg-content">${msg.text}</div>
-            </div>
-        `;
+            // If the message is status just render the message
+            if(msg.type == "status")
+            {
+                document.querySelector(".messages-container").innerHTML +=
+                `
+                <div data-test="message" class="${isStatus}">
+                    <div class="msg-time">${msg.time}</div>
+                    <div class="msg-username">${msg.from}</div>
+                    <div class="msg-content">${msg.text}</div>
+                </div>
+                `;
+            } // If the message is not status add the receiver on it , in this case para Todos <em>
+            else
+            {
+                document.querySelector(".messages-container").innerHTML +=
+                `
+                <div data-test="message" class="${isStatus}">
+                    <div class="msg-time">${msg.time}</div>
+                    <div class="msg-username">${msg.from}</div>
+                    <div class="msg-content">para <em>Todos: </em>${msg.text}</div>
+                </div>
+                `;
+            }
         }
         else
         {
+            // If the message is not for Todos but the sender or reiver or the type of the message is public 
             if(msg.to === myUsername.name || msg.from === myUsername.name || msg.type === "message")
             {
+                // Format the message text to show it to only me or everyone
                 let privateMsgFormat = msg.type == "message" ? "para <em>" + msg.to + "</em>: " + msg.text : "reservadamente para <em>" + msg.to + "</em>: " + msg.text;
+
+                // Check if the message is for only me and color it red and render it
                 let needColorRed = msg.type === "message" ? "" : "private-msg";
                 document.querySelector(".messages-container").innerHTML +=
                 `
@@ -274,27 +361,34 @@ function UpdateMessages(allMessages)
                 </div>
                 `;
             }
-
         }
     });
 
+    // Smooth scrool to new messages
     document.querySelector(".messages-container").lastElementChild.scrollIntoView({ behavior: "smooth" });
 }
 
+// Ask the server for users
 function GetUsers()
 {
     const users = axios.get(loginURL);
+    // If i get the users sucessfully update the render on visibility window else return to login
     users.then(UpdateUsers);
     users.catch(ReturnToLogin);
 }
 
 function UpdateUsers(data)
 {
+    // Assing users data thos this var
     const serverUsers = data.data;
+    // Clear the users from the visibility setting widnow
     document.querySelector(".remittees-container").innerHTML = '';
+    // Check if i had a selected user previously
     const hadSelectedUser = receivers!= 'all';
-    let keepSelectedUser =false;
+    // Create this var to try to keep the selected user after the users refresh
+    let keepSelectedUser = false;
 
+    // If i didnt had a selected user just render the Todos button normally without checkmark on the right
     if(hadSelectedUser == false)
     {
         document.querySelector(".remittees-container").innerHTML +=
@@ -308,6 +402,7 @@ function UpdateUsers(data)
     }
     else
     {
+        // If i had a selected user render the todos button with checkmark hidden
         document.querySelector(".remittees-container").innerHTML +=
         `
             <div data-test="all" onclick="SetReceiver('all')" class="all-users-btn vbtn">
@@ -318,12 +413,16 @@ function UpdateUsers(data)
         `;
     }
 
+    // Foreach user if get on the server data
     serverUsers.forEach ( user =>{
 
+        // If i had a selected user
         if(hadSelectedUser == true)
         {
+            // If the current receivers are not all (i dont know why i check this again)
             if(receivers !='all')
             {
+                // If this user.name is the same i had selected keep the selected user after update
                 if(receivers == user.name)
                 {
                     keepSelectedUser = true;
@@ -331,8 +430,10 @@ function UpdateUsers(data)
             }
         }
         
+        // If this user.name is not myself
         if(user.name != myUsername.name)
         {
+            // If the selected user i had selected !!! DIT NOT !!! stayed on the room after the update
             if(keepSelectedUser == false)
             {
                 document.querySelector(".remittees-container").innerHTML +=
@@ -343,7 +444,7 @@ function UpdateUsers(data)
                         <ion-icon data-test="check" class="checkmark hidden" name="checkmark-sharp"></ion-icon>
                     </div>
                 `;
-            }
+            }  // If the selected user i had selected !! STAYED !! on the room after the update keep the checkmark on it
             else if(user.name == receivers)
             {
                 document.querySelector(".remittees-container").innerHTML +=
@@ -358,6 +459,7 @@ function UpdateUsers(data)
         }
     });
 
+    // If the selected user i had selected !!! DIT NOT !!! stayed on the room after the update reset the default selected user to TODOS
     if(hadSelectedUser && keepSelectedUser == false)
     {
         SetReceiver('all');
